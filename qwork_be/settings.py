dd11c7f3b2fca2ca51134ab871e69863c9699bdd
Quang Tran <quangtv2702@gmail.com>
2024-01-13 13:42:53 +0700

    fix: static

--- qwork_be/settings.py
+++ qwork_be/settings.py
@@ -222,7 +222,6 @@ TIME_ZONE = 'Asia/Bangkok'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
